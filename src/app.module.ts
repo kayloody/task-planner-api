5d92f43c9067cfd6cc04fafe79800d207dd0364c
Khaled <kayloody@gmail.com>
2021-09-09 00:35:40 -0600

    Removed unnecssary services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), TasksModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
